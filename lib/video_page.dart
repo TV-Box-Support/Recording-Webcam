1f31b3328a8abce87f1e68775437410e9a4d0366
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2022-09-19 09:31:00 +0700

    recoding app version 2: done rest api

--- lib/video_page.dart
+++ lib/video_page.dart
@@ -1,16 +1,20 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:camera_app/api/uploadFile.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:video_player/video_player.dart';
-import 'package:http/http.dart' as http;
 
 class VideoPage extends StatefulWidget {
   final String filePath;
   final File file;
 
-  const VideoPage({Key? key, required this.filePath, required this.file})
-      : super(key: key);
+  const VideoPage({
+    Key? key,
+    required this.filePath,
+    required this.file,
+  }) : super(key: key);
 
   @override
   _VideoPageState createState() => _VideoPageState();
@@ -34,12 +38,39 @@ class _VideoPageState extends State<VideoPage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('preview'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+              onPressed: () async {
+                var request =
+                    await uploadFile(context, widget.file).whenComplete(() {
+                  Navigator.of(context).pop();
+                });
+                if (request) {
+                  setState(() {
+                    Fluttertoast.showToast(
+                        msg: "Upload Success!!!!!",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIosWeb: 10,
+                        backgroundColor: Colors.white54,
+                        textColor: Colors.white,
+                        fontSize: 20.0);
+                  });
+                }
+              },
+              icon: const Icon(
+                Icons.cloud_upload,
+                color: Colors.white,
+                size: 35,
+              ))
+        ],
       ),
       // Use a FutureBuilder to display a loading spinner while waiting for the
       // VideoPlayerController to finish initializing.
@@ -63,45 +94,30 @@ class _VideoPageState extends State<VideoPage> {
           }
         },
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              // Wrap the play or pause in a call to `setState`. This ensures the
-              // correct icon is shown.
-              setState(() {
-                // If the video is playing, pause it.
-                if (_videoPlayerController.value.isPlaying) {
-                  _videoPlayerController.pause();
-                } else {
-                  // If the video is paused, play it.
-                  _videoPlayerController.play();
-                }
-              });
-            },
-            // Display the correct icon depending on the state of the player.
-            child: Icon(
-              _videoPlayerController.value.isPlaying
-                  ? Icons.pause
-                  : Icons.play_arrow,
-            ),
-          ),
-          FloatingActionButton(onPressed: () => _uploadFile(widget.file))
-        ],
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_videoPlayerController.value.isPlaying) {
+              _videoPlayerController.pause();
+            } else {
+              // If the video is paused, play it.
+              _videoPlayerController.play();
+            }
+          });
+        },
+        backgroundColor: Colors.white,
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _videoPlayerController.value.isPlaying
+              ? Icons.pause
+              : Icons.play_arrow,
+          color: Colors.black,
+        ),
       ),
     );
-  }
-
-  void _uploadFile(File file) async {
-    var request = http.MultipartRequest(
-        'POST', Uri.parse('http://192.168.1.61:5000/uploadFile'));
-    request.files.add(await http.MultipartFile.fromPath('file', file.path));
-    var response = await request.send();
-    if(response.statusCode == 200 ){
-      print('ok');
-    }else{
-      print('no');
-    };
   }
 }
