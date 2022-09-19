27313fc63e80e796a7a19c87e26a64d531edd623
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2022-09-19 09:33:10 +0700

    recoding app version 2: done rest api

---
+++ lib/api/uploadFile.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<bool> uploadFile(BuildContext context, File file) async {
+  showDialog(
+      // // The user CANNOT close this dialog  by pressing outsite it
+      barrierDismissible: false,
+      context: context,
+      builder: (_) {
+        return Dialog(
+          // The background color
+          backgroundColor: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                // The loading indicator
+                CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                // Some text
+                Text(
+                  'Uploading...',
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+          ),
+        );
+      });
+  var request = http.MultipartRequest(
+      'POST', Uri.parse('http://192.168.1.64:5000/uploadFile'));
+  request.files.add(await http.MultipartFile.fromPath('file', file.path));
+  var response = await request.send();
+
+  // Close the dialog programmatically
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
